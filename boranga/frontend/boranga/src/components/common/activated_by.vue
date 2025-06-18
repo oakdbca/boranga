f08f0ba9c35071aaea5ee00b72abb3da2a08b4de
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-06-18 11:49:57 +0800

    Add new activated_by component.

---
+++ boranga/frontend/boranga/src/components/common/activated_by.vue
@@ -0,0 +1,44 @@
+<template>
+    <div class="card card-default">
+        <div class="card-header">Actviation Details</div>
+        <div class="card-body py-2">
+            <strong>Activated by</strong><br />
+            {{ submitter_first_name }}
+            {{ submitter_last_name }}
+        </div>
+        <div v-if="lodgement_date" class="card-body border-top py-2">
+            <strong>Activated on</strong><br />
+            {{ formatDateDay(lodgement_date) }}
+            <span class="fw-light">at</span>
+            {{ formatDateTime(lodgement_date) }}
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'ActivatedByComponent',
+    props: {
+        submitter_first_name: {
+            type: String,
+            default: '',
+        },
+        submitter_last_name: {
+            type: String,
+            default: '',
+        },
+        lodgement_date: {
+            type: String,
+            default: null,
+        },
+    },
+    methods: {
+        formatDateDay: function (data) {
+            return data ? moment(data).format('DD/MM/YYYY') : '';
+        },
+        formatDateTime: function (data) {
+            return data ? moment(data).format('HH:mm:ss A') : '';
+        },
+    },
+};
+</script>
