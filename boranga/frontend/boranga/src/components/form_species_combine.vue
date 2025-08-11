a6bb73a9dc47779bb861ed647068756fe1297afe
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-08-11 17:04:42 +0800

    Remove unused variable.

--- boranga/frontend/boranga/src/components/form_species_combine.vue
+++ boranga/frontend/boranga/src/components/form_species_combine.vue
@@ -201,7 +201,7 @@ export default {
         },
     },
     watch: {
-        speciesCombineList(list, old) {
+        speciesCombineList(list) {
             if (!list || !list.length) {
                 this.selectedSpeciesIndex = null;
                 return;
