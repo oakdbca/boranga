f1ad9236a0d2f183750aedbe0503e326492cbf52
Oak McIlwain <oak.mcilwain@dbca.wa.gov.au>
2025-08-27 17:12:43 +0800

    Add values that are pre-populated based on the adapter/source.

--- boranga/components/data_migration/adapters/occurrence/tfauna.py
+++ boranga/components/data_migration/adapters/occurrence/tfauna.py
@@ -1,3 +1,6 @@
+from boranga.components.occurrence.models import Occurrence
+from boranga.components.species_and_communities.models import GroupType
+
 from ..base import ExtractionResult, ExtractionWarning, SourceAdapter
 from ..sources import Source
 from . import schema
@@ -16,8 +19,9 @@ class OccurrenceTfaunaAdapter(SourceAdapter):
 
         for raw in raw_rows:
             canonical = schema.map_raw_row(raw)
-            canonical["group_type"] = (
-                "fauna"  # TODO: Add any other source dependent constants here
-            )
+            canonical["group_type"] = GroupType.GROUP_TYPE_FAUNA
+            canonical["occurrence_source"] = Occurrence.OCCURRENCE_CHOICE_OCR
+            canonical["processing_status"] = Occurrence.PROCESSING_STATUS_ACTIVE
+            # TODO: Add any other source dependent constants here
             rows.append(canonical)
         return ExtractionResult(rows=rows, warnings=warnings)
